689dddd69e1391404af6d0503b6bbec950ee9ef2
Hemant <arora85gla@gmail.com>
2023-11-27 04:52:01 +0530

    First Commit

---
+++ src/Package1/Student.java
@@ -0,0 +1,27 @@
+package Package1;
+
+public class Student {
+    int rollNumber;
+    int age;
+    public void display1()
+    {
+        System.out.println("Welcome to all of you ");
+    }
+
+    public void display2()
+    {
+        System.out.println("Automation is very Easy");
+    }
+
+    public static void main(String[] args) {
+        Student s = new Student();
+        s.rollNumber=1;
+        s.age=25;
+        System.out.println("Roll Number : "+s.rollNumber);
+        System.out.println("Age :" + s.age);
+        s.display1();
+        s.display2();
+
+
+    }
+}
